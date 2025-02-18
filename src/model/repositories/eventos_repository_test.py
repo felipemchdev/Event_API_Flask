c89bf4e10939cfff7876a86c22654dc18a7ab269
felipemchdev <felipe.mch.alt@gmail.com>
2025-02-18 14:22:54 -0300

    fixes + features

---
+++ src/model/repositories/eventos_repository_test.py
@@ -0,0 +1,7 @@
+from src.model.repositories.eventos_repository import EventosRepository
+
+def test_insert_eventos():
+    event_name = "eventoTeste2"
+    event_repo = EventosRepository()
+    
+    event_repo.insert(event_name)
