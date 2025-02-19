08bf09c9e0f3278c4639cc9e1a3a32442d24e285
felipemchdev <felipe.mch.alt@gmail.com>
2025-02-19 13:55:20 -0300

    clean code *unused things*

--- src/model/repositories/interface/eventos_link_repository.py
+++ src/model/repositories/interface/eventos_link_repository.py
@@ -4,7 +4,7 @@ from src.model.entities.eventos_link import EventosLink
 class EventosLinkRepositoryInterface(ABC):
 
     @abstractmethod
-    def insert(self, inscrito_id: int, evento_id: int, link: str) -> str: pass
+    def insert(self, inscrito_id: int, evento_id: int) -> str: pass
 
     @abstractmethod
     def select_link(self, inscrito_id: int, evento_id: int) -> EventosLink: pass
